b8753a8a94f59a80472f01e060f2d8f9694b340f
MateuszPoterala <poteralamateusz@gmail.com>
2023-03-05 15:32:42 +0100

    [add] - almost got h2 connection

---
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -0,0 +1,18 @@
+package pl.sda.orange2.jdbc;
+
+import pl.sda.orange2.jdbc.config.H2Config;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+    public static void main(String[] args) {
+
+        try {
+            var h2Connection = DriverManager.getConnection(H2Config.DB_URL, H2Config.USER, H2Config.PASSWORD);
+            System.out.println("Got connection: " + (h2Connection != null));
+        } catch (SQLException e) {
+            System.out.println("No connection!" + e );
+        }
+    }
+}

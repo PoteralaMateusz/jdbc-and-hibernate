35aacc28599844fd932ec999c8b21fa7732e89cf
MateuszPoterala <poteralamateusz@gmail.com>
2023-03-07 20:26:45 +0100

    [fix] - closing database connection

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -10,12 +10,13 @@ public class TestConnection {
                 SELECT ID,NAME
                 FROM TEST
                 """;
+        ResultSet queryResult = null;
         try {
             var h2Connection = DriverManager.getConnection(H2Config.DB_URL, H2Config.USER, H2Config.PASSWORD);
             System.out.println("Got connection: " + (h2Connection != null));
 
             Statement queryStatement = h2Connection.createStatement();
-            ResultSet queryResult = queryStatement.executeQuery(query);
+            queryResult = queryStatement.executeQuery(query);
 
             while (queryResult.next()) {
                 System.out.println("id: " + queryResult.getInt(1));
@@ -25,6 +26,14 @@ public class TestConnection {
         } catch (SQLException e) {
             System.out.println("No connection!" + e);
             e.printStackTrace();
+        } finally {
+            if (queryResult != null) {
+                try {
+                    queryResult.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }

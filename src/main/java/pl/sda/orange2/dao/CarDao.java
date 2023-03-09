5ca06153e43b71c29c00f1065ba66aa370e2a336
MateuszPoterala <poteralamateusz@gmail.com>
2023-03-09 19:35:33 +0100

    [change] - findAll and findByID dao methods implemented

--- src/main/java/pl/sda/orange2/dao/CarDao.java
+++ src/main/java/pl/sda/orange2/dao/CarDao.java
@@ -2,10 +2,17 @@ package pl.sda.orange2.dao;
 
 import pl.sda.orange2.entity.Car;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CarDao implements DataAccess<Car,Long> {
+public class CarDao implements DataAccess<Car, Long> {
 
+    private final Connection dbConnection;
+
+    public CarDao(Connection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
 
     @Override
     public void save(Car entity) {
@@ -14,12 +21,60 @@ public class CarDao implements DataAccess<Car,Long> {
 
     @Override
     public List<Car> findAll() {
-        return null;
+        var cars = new ArrayList<Car>();
+        String allCarsQuery = """
+                SELECT ID, COLOUR, BRAND, MODEL
+                FROM CARS;
+                """;
+        try {
+            Statement queryStatement = dbConnection.createStatement();
+            ResultSet queryResult = queryStatement.executeQuery(allCarsQuery);
+
+            while (queryResult.next()) {
+                Long id = queryResult.getLong(1);
+                String colour = queryResult.getString(2);
+                String brand = queryResult.getString(3);
+                String model = queryResult.getString(4);
+
+                Car carFromDb = new Car(id, colour, brand, model);
+                cars.add(carFromDb);
+            }
+        } catch (SQLException e) {
+            System.out.println("Unexpected sql exception occurred");
+            e.printStackTrace();
+        }
+
+        return cars;
     }
 
     @Override
-    public Car findById(Long aLong) {
-        return null;
+    public Car findById(Long id) {
+        Car result = null;
+        String carByIdQuery = """
+                SELECT ID, COLOUR, BRAND, MODEL
+                FROM CARS
+                WHERE ID = ?
+                """;
+        try {
+            PreparedStatement queryStatement = dbConnection.prepareStatement(carByIdQuery);
+            queryStatement.setLong(1, id);
+            ResultSet queryResult = queryStatement.executeQuery();
+
+            if (queryResult.next()){
+                result = new Car(
+                        queryResult.getLong("ID"),
+                        queryResult.getString("COLOUR"),
+                        queryResult.getString("BRAND"),
+                        queryResult.getString("MODEL")
+                );
+            }
+
+        } catch (
+                SQLException e) {
+            System.out.println("Unexpected sql exception occurred");
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
---
+++ src/main/java/pl/sda/orange2/jdbc/CarDaoExample.java
@@ -0,0 +1,33 @@
+package pl.sda.orange2.jdbc;
+
+import pl.sda.orange2.dao.CarDao;
+import pl.sda.orange2.entity.Car;
+import pl.sda.orange2.jdbc.config.H2Config;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CarDaoExample {
+
+    public static void main(String[] args) {
+        System.out.println("Car dao example");
+
+        System.out.println("Reading all cars from db");
+
+        try {
+            var h2Connection = DriverManager.getConnection(H2Config.DB_URL, H2Config.USER, H2Config.PASSWORD);
+            CarDao carDao = new CarDao(h2Connection);
+            var allCarsFromDb = carDao.findAll();
+            System.out.println("All cars form db " + allCarsFromDb);
+
+            System.out.println("Finding car by id");
+            System.out.println("First with existing id");
+            Car existing = carDao.findById(1L);
+            System.out.println(existing);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}

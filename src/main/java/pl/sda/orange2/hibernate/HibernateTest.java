300607562365706f3bb36ad1fcb14f00a43dd129
MateuszPoterala <poteralamateusz@gmail.com>
2023-03-13 18:50:01 +0100

    [add] - test getting students data from database in HibernateTest

--- src/main/java/pl/sda/orange2/hibernate/HibernateTest.java
+++ src/main/java/pl/sda/orange2/hibernate/HibernateTest.java
@@ -5,19 +5,33 @@ import org.hibernate.Transaction;
 import pl.sda.orange2.entity.Student;
 import pl.sda.orange2.hibernate.util.HibernateUtil;
 
+import java.util.List;
+
 public class HibernateTest {
 
     public static void main(String[] args) {
 
-        Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
-        Student student1 = new Student("John", "Cena", "john@javaguides.com");
+        Student student = new Student("Mateusz", "Poterala", "mateusz@gmail.com");
+        Student student1 = new Student("Jan", "Kowalski", "jan@gmail.com");
 
         Transaction transaction = null;
+        // add some students to database
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.persist(student);
             session.persist(student1);
             transaction.commit();
+        } catch (Exception e){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        // gets students from database
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List<Student> students = session.createQuery("from Student", Student.class).getResultList();
+            students.forEach(System.out::println);
         } catch (Exception e){
             if (transaction != null){
                 transaction.rollback();
